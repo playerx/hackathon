456dfa3a28d7083589edc3c67ceba1578ed1616e
Ezeki Zibzibadze <ez@jok.io>
2022-11-05 17:32:24 -0700

    feat: automate login without additional clicks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
-import React, { useContext } from "react";
-import { WalletContext } from "../contexts/WalletContext";
-import { shortAddress } from "../utils/utils";
+import React, { useContext, useEffect } from "react";
 import xmtpLogo from "../assets/xmtp-logo.png";
+import { WalletContext } from "../contexts/WalletContext";
 import { XmtpContext } from "../contexts/XmtpContext";
+import { shortAddress } from "../utils/utils";
 
 const Header = () => {
   const { connectWallet, walletAddress, signer } = useContext(WalletContext);
   const [providerState] = useContext(XmtpContext);
+
+  useEffect(() => {
+    const hasKeys = !!localStorage.getItem("keys");
+    if (hasKeys) {
+      connectWallet();
+    }
+  }, []);
+
+  useEffect(() => {
+    const hasKeys = !!localStorage.getItem("keys");
+
+    if (hasKeys && signer) {
+      providerState.initClient(signer);
+    }
+  }, [signer, providerState.initClient]);
 
   return (
     <div className="header flex align-center justify-between">
